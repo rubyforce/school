d29b1931b8daad95272fdb45e52dfc62c386fafc
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-04-08 11:27:57 +0300

    added create edit and destroy method for employee class

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,4 +2,43 @@ class EmployeesController < ApplicationController
 	def index
 		@employees = Employee.all
 	end
+
+	def show
+		@employee = Employee.find(params[:id])
+	end
+
+	def new
+		@employee = Employee.new
+	end
+
+	def create
+		@employee = Employee.new(params[:employee])
+
+		if @employee.save
+			redirect_to employees_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@employee = Employee.find(params[:employee])
+	end
+
+	def update
+		@employee = Employee.find(params[:employee])
+
+		if @employee.update_attributes(params[:employee])
+			redirect_to employees_path
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@employee = Employee.find(params[:employee])
+		@employee.destroy
+
+		redirect_to employees_path
+	end
 end
