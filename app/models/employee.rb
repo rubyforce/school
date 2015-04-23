97b83ccdca5f75c3e1264e5954d634a445fd2e0b
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-04-23 12:49:12 +0300

    fixing totally fail

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,7 @@
 class Employee < ActiveRecord::Base
-	attr_accessible :first_name, :last_name, :job_title, :status, :gender, :address,
-                  :middle_name, :qualification, :salary, :contact_no, :entry_no,
-                  :datetime, :paid_type, :section, :pay_band, :salary_check, :remark,
-									:paid_type_id, :section_id, :pay_band_id, :year
-
-
+	attr_accessible :first_name, :last_name, :job_title, :status, :gender, :address, :middle_name,
+                  :qualification, :salary, :contact_no, :entry_no, :datetime, :salary_check,
+                  :remark,:paid_type_id, :section_id, :pay_band_id, :year
 
   belongs_to :cash_management
 	belongs_to :paid_type
