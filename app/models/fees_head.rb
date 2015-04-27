80339f330911947c09ff01a1f1ef8a71462b3f36
Alexandr Korsak <alex.korsak@gmail.com>
2015-04-27 13:52:58 +0300

    Clean up fees head

--- app/models/fees_head.rb
+++ app/models/fees_head.rb
@@ -2,7 +2,7 @@ class FeesHead < ActiveRecord::Base
   attr_accessible :name, :amount, :type, :reccurence, :class, :division, :student_name
 
   has_many :students_fees_heads
-  has_many :student, through: :students_fees_heads
+  has_many :students, through: :students_fees_heads
 
   accepts_nested_attributes_for :students
 end
