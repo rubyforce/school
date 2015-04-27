ebc98e687e0bcfbf7667d04f25ccbfc9d2974ea2
Alexandr Korsak <alex.korsak@gmail.com>
2015-04-27 13:46:53 +0300

    remove unused files

--- spec/controllers/natures_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe NaturesController do
-
-end
--- spec/models/nature_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Nature do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
