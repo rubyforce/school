62f80d5a620a006ff3acac7315a8acd3a3e9e6b5
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2015-05-25 18:18:50 +0300

    Total salary expense for the last month

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -10,7 +10,8 @@ class Admin::DashboardsController < ApplicationController
       :employees_govt_pay => Employee.joins(:paid_type).where(paid_types: { title: 'Government pay' }),
       :employees_section => Employee.joins(:section).where(sections: { title: 'Non-teaching' }),
       :expense => ExpenseReceipt.last.amount,
-      :expense_current_month => ExpenseReceipt.where("EXTRACT(MONTH FROM created_at) = ?", Date.today.month).sum(:amount)
+      :expense_current_month => ExpenseReceipt.where("EXTRACT(MONTH FROM created_at) = ?", Date.today.month).sum(:amount),
+      :salary_expenses_last_month => EmployeeSalaryReceipt.joins(:salary_receipt).where("EXTRACT(MONTH FROM month) = ?", Date.today.month.to_date-1).sum(:salary)
     }
   end
 end
