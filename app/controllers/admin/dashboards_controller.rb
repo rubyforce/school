2f40a3be663948993e5228ea3327bfe8d778e2ef
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2015-05-25 19:20:23 +0300

    Dashboard Fees collection

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -11,7 +11,11 @@ class Admin::DashboardsController < ApplicationController
       :employees_section => Employee.joins(:section).where(sections: { title: 'Non-teaching' }),
       :expense => ExpenseReceipt.where("EXTRACT(DAY FROM created_at) = ?", Date.today.day).sum(:amount),
       :expense_current_month => ExpenseReceipt.where("EXTRACT(MONTH FROM created_at) = ?", Date.today.month).sum(:amount),
-      :salary_expenses_last_month => EmployeeSalaryReceipt.joins(:salary_receipt).where("EXTRACT(MONTH FROM month) = ?", Date.today.month-1).sum(:salary)
+      :salary_expenses_last_month => EmployeeSalaryReceipt.joins(:salary_receipt).where("EXTRACT(MONTH FROM month) = ?", Date.today.month-1).sum(:salary),
+
+      :today_fees_collection => Receipt.where("EXTRACT(DAY FROM created_at) = ?", Date.today.day).sum(:cash),
+      :current_month_fees_collection => Receipt.where("EXTRACT(MONTH FROM created_at) = ?", Date.today.month).sum(:cash),
+      :last_month_fees_collection => Receipt.where("EXTRACT(MONTH FROM created_at) = ?", Date.today.month-1).sum(:cash)
     }
   end
 end
