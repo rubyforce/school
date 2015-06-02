332ffc214b452fc23cba61e5a6ac7f0fcd3bb15c
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2015-06-02 12:31:29 +0300

    Assign standard to fees_heads

--- app/controllers/admin/standards_controller.rb
+++ app/controllers/admin/standards_controller.rb
@@ -3,13 +3,13 @@ class Admin::StandardsController < ApplicationController
 
   def index
     @standards = Standard.all
-    respond_with @standards
+    render :json => @standards.as_json(:include => :standards_fees_heads)
   end
 
   def show
     @standard = Standard.find(params[:id])
 
-    render :json => @standard
+    render :json => @standard.as_json(:include => :standards_fees_heads)
   end
 
   def new
@@ -17,20 +17,26 @@ class Admin::StandardsController < ApplicationController
   end
 
   def create
-    @standard = Standard.new(params[:standard])
+    @standard = Standard.new(attributes)
+    if params[:standard][:standards_fees_heads_attributes].blank?
+      params[:standard].delete(:standards_fees_heads_attributes)
+    end
     @standard.save
 
-    render :json => @standard
+    render :json => @standard.as_json(:include => :standards_fees_heads)
   end
 
   def edit
     @standard = Standard.find(params[:id])
-    render :json => @standard
+    render :json => @standard.as_json(:include => :standards_fees_heads)
   end
 
   def update
     @standard = Standard.find(params[:id])
-    @standard.update_attributes(params[:standard])
-    render :json => @standard
+    if params[:standard][:standards_fees_heads_attributes].blank?
+      params[:standard].delete(:standards_fees_heads_attributes)
+    end
+    @standard.update_attributes(attributes)
+    render :json => @standard.as_json(:include => :standards_fees_heads)
   end
 end
