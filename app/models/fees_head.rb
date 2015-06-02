08d02ac9f320bb810d2e9cae932552a28752a15c
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2015-06-02 04:19:44 +0300

    Assign fees_head to multiple classes

--- app/models/fees_head.rb
+++ app/models/fees_head.rb
@@ -2,8 +2,13 @@ class FeesHead < ActiveRecord::Base
   has_many :students_fees_heads
   has_many :students, through: :students_fees_heads
 
+  has_many :standards_fees_heads
+  has_many :standards, through: :standards_fees_heads
+
   belongs_to :fees_head_type
   belongs_to :fees_head_recurrence
 
   accepts_nested_attributes_for :students
+
+  accepts_nested_attributes_for :standards
 end
