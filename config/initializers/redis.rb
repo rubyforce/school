389599f24c753564b333c8b8179439a2b52f02b9
Alexandr Korsak <alex.korsak@gmail.com>
2015-07-15 18:51:25 +0300

    Remove unused stuff

--- config/initializers/redis.rb
+++
@@ -1,7 +0,0 @@
-redis_uri = URI.parse(Rails.configuration.redis.url)
-
-$redis = Redis.new(:host => redis_uri.host,
-                   :port => redis_uri.port,
-                   :password => redis_uri.password,
-                   :thread_safe => true)
-
