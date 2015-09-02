fff0633298c1e77724642cf5265fac7b59f0374a
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2015-09-02 10:35:47 +0300

    merge

---
+++ spec/models/daily_meal_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe DailyMeal do
+  # context "Before save" do
+  #   let(:daily_meal_meal1) { create(:daily_meal_meal, daily_meal: daily_meal) }
+  #   let(:daily_meal_meal2) { create(:daily_meal_meal, daily_meal: daily_meal) }
+  #   let(:daily_meal) { build(:daily_meal) }
+
+  #   before { daily_meal.save }
+
+  #   it "should assigned amount of students" do
+  #     expect(daily_meal.no_of_student1).to eq(10)
+  #     expect(daily_meal.no_of_student2).to eq(15)
+  #     expect(daily_meal.no_of_student3).to eq(20)
+  #     expect(daily_meal.no_of_student4).to eq(25)
+  #     expect(daily_meal.no_of_student5).to eq(30)
+  #   end
+  # end
+end
